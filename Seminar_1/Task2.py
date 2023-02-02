4b2511406c4cf4c3c044790af70df90c8554f0e8
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-02-02 15:46:36 +0500

    Add Task 2

--- Seminar_1/Task2.py
+++ Seminar_1/Task2.py
@@ -3,3 +3,14 @@
 # Пример:
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+def sum_digit(number):
+    sum = 0
+    while number > 0:
+        sum += number % 10
+        number //= 10
+    return sum
+
+num = int(input('Введите число - '))
+sum = sum_digit(num)
+print(f'Сумма цифр числа {num} = {sum}')
