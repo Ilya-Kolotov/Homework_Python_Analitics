b28ed722d619dcf98c33dbc5aadbfc2e5639f1ac
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-02-06 16:46:08 +0500

    Update Task 18

--- Seminar_3/Task18.py
+++ Seminar_3/Task18.py
@@ -7,33 +7,69 @@
 # 1 2 3 4 5
 # 6
 # -> 5
-from  random import randint
 
-def input_numbers(input_text):
-    while True:
-        try:
-            number = int(input(input_text))
-            return number
-        except ValueError:
-            print('Ошибка! Введите число')
+# ver 1
 
+# def input_numbers(input_text):
+#     while True:
+#         try:
+#             number = int(input(input_text))
+#             return number
+#         except ValueError:
+#             print('Ошибка! Введите число')
+#
+#
+# def check_elem(list_check):
+#     if x >= list_check[-1]:
+#         check_num = list_check[-1]
+#         return check_num
+#     elif x <= list_check[0]:
+#         check_num = list_check[0]
+#         return check_num
+#     else:
+#         for i in range(1,len(list_check) - 1):
+#             if x == list_check[i]:
+#                 check_num = list_check[i]
+#                 return check_num
+#
+# n = input_numbers('Введите количество элементов в массиве - ')
+# x = input_numbers(f'Введите число, которое надо проверить - ')
+# list_1 = [i for i in range(1, n + 1)]
+# check_num = check_elem(list_1)
+# print(n)
+# print(*list_1)
+# print(x)
+# print(f'Ближайший элемент по величине близкий к {x} = {check_num}')
 
-def check_elem(list_check):
-    diff = []
-    for j in range(len(list_check)):
-            diff.append(abs(x - list_check[j]))
-    check_num = 0
-    for i in range(len(diff)):
-        if diff[i] == min(diff):
-            check_num = list_check[i]
-    return check_num
+# ver 2 with random
 
-n = input_numbers('Введите количество элементов в массиве - ')
-x = input_numbers(f'Введите число, которое надо проверить - ')
-list_1 = [randint(1, n) for i in range(n)]
-check_num = check_elem(list_1)
-
-print(n)
-print(*list_1)
-print(x)
-print(f'Ближайший элемент по величине близкий к {x} = {check_num}')
+# from  random import randint
+#
+# def input_numbers(input_text):
+#     while True:
+#         try:
+#             number = int(input(input_text))
+#             return number
+#         except ValueError:
+#             print('Ошибка! Введите число')
+#
+#
+# def check_elem(list_check):
+#     diff = []
+#     check_num = []
+#     for j in range(len(list_check)):
+#             diff.append(abs(x - list_check[j]))
+#     for i in range(len(diff)):
+#         if diff[i] == min(diff):
+#             check_num.append(list_check[i])
+#     return check_num
+#
+#
+# n = input_numbers('Введите количество элементов в массиве - ')
+# x = input_numbers(f'Введите число, которое надо проверить - ')
+# list_1 = [randint(1, n) for i in range(n)]
+# check_num = set(check_elem(list_1))
+# print(n)
+# print(*list_1)
+# print(x)
+# print(f'Ближайший(-е) элемент(-ы) по величине близкий к {x} =', ','.join(map(str, check_num)))
